2840b4bfbc593c21ac815da0bb2ab7d9f5785fe5
Molinux <cybercrowbr@gmail.com>
2020-06-04 22:13:22 -0300

    Modal to find colect points. Collect items select

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,11 +24,17 @@ function getCities(event) {
 
   const url =`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+  // Ao escolher um novo estado, limpar o select de cidades, pois ao trocar de estado
+  // estava mantendo as cidades do estado anterior em cache
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
+
   fetch(url)
     .then(res => res.json())
     .then(cities => {
+
       for ( const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
       
       citySelect.disabled = false;
@@ -39,3 +45,55 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities);
+
+// Itens de coleta
+// Todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+  // add or remove a class
+  itemLi.classList.toggle("selected")
+
+  const itemId = event.target.dataset.id
+
+  
+  // Check item selected
+  // Get selected items
+  // const alreadySelected = selectedItems.findIndex(item => item == itemId)
+  const alreadySelected = selectedItems.findIndex(item => {
+    const itemFound = item == itemId
+    return itemFound
+  })
+
+  console.log(alreadySelected)
+
+  //If selected, unselect it
+  if(alreadySelected >= 0) {
+    // remove from selectdItems
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+
+    selectedItems = filteredItems
+  } else {
+    // If unselected, add it to array
+    selectedItems.push(itemId)
+
+  }
+  
+  // Update hidden field with selected items
+  collectedItems.value = selectedItems
+
+  console.log(selectedItems)
+
+}
